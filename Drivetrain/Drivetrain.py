ce6cd03f394b1ebd5448f5e8f9dced0beb1fe7b6
Joep <Joepdeboer-3@student.tudelft.nl>
2023-06-13 19:30:18 +0200

    Added function to calculate main requirements of the drive-train from cable_force profile and cable_velocity profile

--- Drivetrain/Drivetrain.py
+++ Drivetrain/Drivetrain.py
@@ -1,46 +1,60 @@
+from typing import Callable
+
 import numpy as np
+import scipy as sc
 from parapy.core import*
 from MotorInverter import MotorInverter
 from Gear import Gear
 from ElectricalMotor import ElectricalMotor
 from Drum import Drum
+import OperationParameters as op
 
 
 class Drivetrain(Base):
-    powerprofiles: np.ndarray = Input()  # torque-velocity curve cubic spline??
+    velocityprofile: Callable = Input()  # callable velocity-time function with input t[s] ,output v in [m/s]
+    cableforceprofile: Callable = Input()  # Callable Force-time function with input t [s],output force in [N]
     starts_per_hour: float = Input()
 
     @Attribute
     def numberofdrums(self):  # TODO Implment number of drums based on starts per hour
-        coupling = 30  # [s] average_time of coupling a plane
-        tightening = 10 # [s] time to tighten the rope
-        launchtime = 30 # [s] average_time from ground to decoupling of the plane
-        spoolingin  = 20 # [s] time
-        spoolingout_speed = 30 / 3.6 # [m/s] average speed of car pulling the cables to the planes
-        winchfieldlenght = 1000 # [m]
-        average_startime = 3600/self.starts_per_hour # required average start time to get enough starts per hour
-        Constant_handeling_time = coupling + tightening + launchtime + spoolingin
 
-        drums = ((average_startime - (tightening + launchtime + spoolingin)) * (
-                    spoolingout_speed / winchfieldlenght)) ** -1
+        average_startime = 3600/self.starts_per_hour # required average start time to get enough starts per hour
+        Constant_handeling_time = op.coupling + op.tightening + op.launchtime + op.spoolingin
+
+        drums = ((average_startime - Constant_handeling_time) * (op.spoolingout_speed / op.winchfieldlenght)) ** -1
+
         if average_startime <= Constant_handeling_time or drums > 8:
             print("The amount of starts per hour is unfeasible, the amount of drums will be fixed at TBD") # TODO TBD
             drums = 6
-        return drums
+        return round(drums +1)
 
     @Attribute
     def minimal_drivetrainrequirements(self):
-        # TODO add calculation based on power profiles what the minimal drive_train req are
-        requirements = {'max_torque': None,  # max of spline
-                        'max_velocity': None,  # max radial cable velocity
-                        'peak_power': None,   # power
-                        'continous_power': None,  # power to be sustained longer than 3 seconds
+        def power(t: float, force_func: Callable, velocity_func: Callable) -> Callable:
+            return force_func(t) * velocity_func(t)
+
+        #Finding max power by finding the power interval and doing bisection
+        x_0 = 30  # estimate 1 of when the winch start including reeling in is over
+        x_1 = 60  # estimate 2 of when the winch start including reeling in is over
+        t_max = sc.optimize(power, x_0, fprime=None, args = (self.cableforceprofile, self.velocityprofile), x1 = x_1, tol = 1)
+        max_power = -sc.optimize.golden(-1*(power), args =(self.cableforceprofile, self.velocityprofile))
+        max_force = -sc.optimize.golden(-1*(self.cableforceprofile))
+        max_velo  = -sc.optimize.golden(-1*(self.velocityprofile))
+        continous_power  = sc.integrate.quad(power, 0, t_max)[0]/t_max
+
+
+        requirements = {'max_force':  max_force,  # max of spline
+                        'max_velocity': max_velo,  # max radial cable velocity
+                        'peak_power': max_power,   # power
+                        'continous_power': continous_power,  # power to be sustained longer than 3 seconds
                         }
         return requirements
 
+
+
     @Part
     def drum(self):
-        pass
+        return Drum()
 
     @Part
     def gear(self):
--- OperationParameters.py
+++ OperationParameters.py
@@ -11,3 +11,9 @@ spoolingout_speed = 30 / 3.6 # [m/s] average speed of car pulling the cables to 
 
 # Airstrip Constants
 winchfieldlenght = 1000 # [m]
+
+
+# Safety parameters
+max_requiredforce = 20 * 10**3 * 0.9  # Max force [N] aplied to the cable set to 90% strongest weaklink (most two-seaters)
+
+
