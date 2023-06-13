ce939a5040132bed58c117058e658239cb5871ff
Joep <Joepdeboer-3@student.tudelft.nl>
2023-06-13 11:47:04 +0200

    Created drive train method for calculating required amount of drums. Additionally started a OperationsParameters.py file which will contain constants such as winchfield lenght, groundhandeling time parameers and more so no hardcoded constants are required in the python files containing the classes

--- Drivetrain/Gear.py
+++ Drivetrain/Gear.py
@@ -1,12 +1,18 @@
 from parapy.core import*
 class Gear(Base):
-    gearratio = Input()
-    torque = Input()
-    gear_type = Input() # spur, Helical, planetary
-    # TODO add statistics for weight and efficiency based on type, torque and speed
+    gearratio = Input()  # motorpm/axelrpm
+    max_torque = Input()
+    max_rpm = Input()
+    operating_angle = Input(0)  # Angle between motor and axel
+
+    @Attribute
+    def gear_type(self):  # TODO implement rule to decide gear type based on ratio, max_torque and max rpm
+        gear_type = 'Helical'  # spur, Helical, planetary, bevel
+        return gear_type
 
     @Attribute
     def mass(self):
+        # TODO add statistics for weight and efficiency based on type, torque and speed
         return None
 
     @Attribute
