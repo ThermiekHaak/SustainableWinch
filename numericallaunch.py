31fa92d06dadf1f26b8c051f3276febc8feb8424
Joep <Joepdeboer-3@student.tudelft.nl>
2023-06-15 23:31:07 +0200

    finished the force speed thingy

--- numericallaunch.py
+++ numericallaunch.py
@@ -1,3 +1,5 @@
+import numpy
+import scipy.integrate
 from  scipy import optimize
 import numpy as np
 import matplotlib.pyplot as plt
@@ -52,32 +54,68 @@ def Fc(W, V, glideratio, gamma, gamma_prime, Beta): # discontinous at x = π n +
     m = W/9.81
     return (m * V * gamma_prime + W * np.cos(gamma))/glideratio/(np.cos(gamma + Beta) - np.sin(gamma + Beta)/glideratio)
 
+def Rprime(x: float, wl: float, alt: float, cut_off: float, V: float):
+    g = path(x, wl, alt, cut_off)
+    g_prime = path_prime(x, wl, alt, cut_off)
+    x_prime = np.cos(g) * V
+    return (g_prime * x + g * x_prime + (wl - x) * x_prime)/np.sqrt((g**2 + (wl - x)**2))
+
+def newCableVelocity(x,y,t,wl):
+    cablelenght = np.sqrt((x - wl) ** 2 + y ** 2)
+    delta = cablelenght[:-1] - cablelenght[1:]
+    deltat = t[1:] - t[:-1]
+    V = np.divide(delta,deltat)
+    V2 = list(V)
+    V2.append(V[-1])
+    # V2.insert(0,V[0])
+    return V2
+
+def time(x: np.ndarray, wl: float, alt: float, cut_off: float, V: float):
+    t = [0]
+    def pathlenght(x, wl, alt, cut_off):
+        y_prime = path_prime(x, wl, alt, cut_off)
+        return np.sqrt(1 + y_prime)
+    for i in x[1:]:
+        l = scipy.integrate.quad(pathlenght, 0, i, args=(wl, alt, cut_off))[0]
+        ti = l/V
+        t.append(ti)
+    return np.array(t)
+
+
+
+
 
 if __name__ == "__main__":
     W = 826* 9.81
     V = 110/3.6
     glideratio = 42
     wl = 1000
-    alt = 510
+    alt = 450
     cut_off = 80
 
     x_max = round(wl - alt/np.tan(8/18 * np.pi) + 1)
     x = np.arange(0, x_max, .1)
     y = path(x, wl, alt, cut_off= cut_off)
+    t = time(x, wl, alt, cut_off, V)
     g = gamma(x, wl, alt, cut_off)
     g_prime = gamma_prime(x, wl, alt, cut_off)
     B = beta(x, y, wl)
     Fc = Fc(W, V, glideratio, g, g_prime, B)
-    #plt.plot(x, path(x, 1500, 500, 80))
-    plus = g + B
-    dis = (plus - np.arctan(glideratio))%np.pi
-    # plt.plot(x, B , color = 'r')
-    # plt.plot(x, g+B - np.arctan(glideratio), color = 'purple')
-    # plt.plot(x, g, color = 'pink')
-    # plt.plot(dis, B, color = 'b')
+    cable_vel = newCableVelocity(x,y,t,wl)
+    #cable_vel = Rprime(x, wl, alt, cut_off, V)
 
-    plt.plot(x, Fc, color = 'r')
+    # plt.plot(x,y , color ='r')
+    plt.plot(t,y, color ='blue')
+    plt.plot(t,x, color = 'pink')
+    plt.plot(t,Fc, color ='green')
+    plt.plot(t,cable_vel, color = 'purple')
+    # plt.plot(cable_vel*Fc, t, color = 'black')
     plt.show()
+
+
+
+
+
 
 
 
